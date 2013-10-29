0a27f1259d1a0aa99699e0bdcf1fa67f3dc5661e
Sven Petersen <s.petersen@gmx.net>
2013-10-29 22:03:06 +0100

    nochmal mit noch weniger fehlern

--- breefkasten.php
+++ breefkasten.php
@@ -44,13 +44,13 @@
             <div class="row-fluid inhalt">
                 <div class="span12">
                     <article>
-                        <p>Up düsse Siede dot stehen, wie de meer wat vertellen kunst.</p>
+                        <p>Up düsse Siede dot stein, as du mi wat vertellen kannst.</p>
 
-                        <p>Dat nennt söch <strong> A Standard for the Transmission of IP Datagrams on Avian Carriers</strong>. 
+                        <p>Dat noemt söch <strong> A Standard for the Transmission of IP Datagrams on Avian Carriers</strong>. 
                         Beesrieven is dat allens in dössen 
                         <a class="btn btn-info" href='http://www.ietf.org/rfc/rfc1149.txt' target='_blank'>RFC 1149</a>. 
                          Un in <a class="btn btn-info" href='http://www.ietf.org/rfc/rfc2549.txt' target='_blank'>RFC 2549</a> is och noch wat drin.</p>
-                        <p>Nich dat de denkst, ik veertell die nur Tüddelkrom. Dee Bengels 
+                        <p>Nich dat de denkst, ik veertell di nur Tüddelkrom. De Buttchers un Deerns  
                         <a class="btn btn-info" href='http://www.blug.linux.no/rfc1149/' target='_blank'>hier</a> hebben döt all daan.</p>
                     </article>
                 </div>
